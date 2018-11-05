dca443b58eede1899d8baf7a0bfe7747aff7e5e1
jszymk <janusz.szymkowiak1@gmail.com>
2018-11-05 02:34:38 +0100

    test fix

--- src/test/java/integration/IntegrationTest.java
+++ src/test/java/integration/IntegrationTest.java
@@ -1,9 +1,6 @@
 package integration;
 
 import app.Application;
-import app.utils.JsonUtils;
-import com.fasterxml.jackson.core.type.TypeReference;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,17 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
-import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
-import java.math.BigDecimal;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Transactional
 @RunWith(SpringRunner.class)
@@ -31,7 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @TestPropertySource(
         locations = "classpath:application-test.properties")
-public class IntegrationTest {
+public abstract class IntegrationTest {
 
     @Autowired
     protected MockMvc mockMvc;
