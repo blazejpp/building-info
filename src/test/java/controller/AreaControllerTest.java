cd2d16a45d36e33bdede680ae46a4552254ca217
Haradd <blazej.piaskowski@gmail.com>
2018-12-18 15:55:28 +0100

    add AreaController test

--- src/test/java/controller/AreaControllerTest.java
+++ src/test/java/controller/AreaControllerTest.java
@@ -19,7 +19,7 @@ public class AreaControllerTest extends IntegrationTest {
     private static final String AREA_ENDPOINT_PATH = "/area/";
 
     @Test
-    public void shouldCalculateArea() throws Exception {
+    public void shouldCorrectlyCalculateBuildingArea() throws Exception {
 
         MvcResult mvcResult = mockMvc.perform(get(AREA_ENDPOINT_PATH + "building/1"))
                 .andExpect(status().isOk())
@@ -27,7 +27,40 @@ public class AreaControllerTest extends IntegrationTest {
 
         BigDecimal result = JsonUtils.fromJson(mvcResult.getResponse().getContentAsString(), new TypeReference<BigDecimal>(){});
 
-        assertThat("area calculated", result, is(equalTo(BigDecimal.valueOf(90))));
+        assertThat("building area calculated", result, is(equalTo(BigDecimal.valueOf(90))));
     }
+
+    @Test
+    public void shouldCorrectlyCaculateFloorArea() throws Exception
+
+    {
+        String building_id = "1";
+        String floor_number = "1";
+        MvcResult mvcResult = mockMvc.perform(get(AREA_ENDPOINT_PATH + "building/" + building_id + "/floor/" + floor_number))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        BigDecimal result = JsonUtils.fromJson(mvcResult.getResponse().getContentAsString(), new TypeReference<BigDecimal>() {
+        });
+
+        assertThat("floor area calculated", result, is(equalTo(BigDecimal.valueOf(30))));
+    }
+
+    @Test
+    public void shouldCorrectlyCaculateRoomArea() throws Exception
+
+    {
+        String building_id = "1";
+        String room_number = "1";
+        MvcResult mvcResult = mockMvc.perform(get(AREA_ENDPOINT_PATH + "building/" + building_id + "/room/" + room_number))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        BigDecimal result = JsonUtils.fromJson(mvcResult.getResponse().getContentAsString(), new TypeReference<BigDecimal>() {
+        });
+
+        assertThat("room area calculated", result, is(equalTo(BigDecimal.valueOf(10))));
+    }
+
 
 }
