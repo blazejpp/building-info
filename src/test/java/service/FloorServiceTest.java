7210aef50170f10ba85da6f299decdda77f4668f
Haradd <blazej.piaskowski@gmail.com>
2018-12-18 15:18:09 +0100

    add new FloorService tests

--- src/test/java/service/FloorServiceTest.java
+++ src/test/java/service/FloorServiceTest.java
@@ -13,6 +13,7 @@ import org.mockito.MockitoAnnotations;
 import javax.annotation.Resource;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,20 +32,6 @@ public class FloorServiceTest {
     @Resource
     private FloorService floorService;
 
-    @Before
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
-    }
-
-    @Test
-    public void shouldCalculateArea() {
-        when(roomService.calculateArea(any())).thenReturn(getArea());
-
-        BigDecimal result = floorService.calculateArea(getFloor());
-
-        assertThat("area", result, is(equalTo(BigDecimal.valueOf(5))));
-    }
-
     private Floor getFloor() {
         List<Room> rooms = new ArrayList<>();
         for(int i = 0; i<5; ++i) {
@@ -59,6 +46,85 @@ public class FloorServiceTest {
 
     private BigDecimal getArea() {
         return BigDecimal.ONE;
+    }
+    private BigDecimal getVolume() {
+        return BigDecimal.ONE;
+    }
+    private BigDecimal getLighting() {
+        return BigDecimal.ONE;
+    }
+    private BigDecimal getHeating() {
+        return BigDecimal.ONE;
+    }
+
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void shouldCalculateArea() {
+        when(roomService.calculateArea(any())).thenReturn(getArea());
+
+        BigDecimal result = floorService.calculateArea(getFloor());
+
+        assertThat("area", result, is(equalTo(BigDecimal.valueOf(5))));
+    }
+
+
+    @Test
+    public void shouldCalculateVolume() {
+        when(roomService.calculateVolume(any())).thenReturn(getVolume());
+
+        BigDecimal result = floorService.calculateVolume(getFloor());
+
+        assertThat("volume", result, is(equalTo(BigDecimal.valueOf(5))));
+
+    }
+
+    @Test
+    public void shouldCalculateLighting() {
+        when(roomService.calculateLighting(any())).thenReturn(getLighting());
+
+        BigDecimal result = floorService.calculateLighting(getFloor());
+
+        assertThat("lightning", result, is(equalTo(BigDecimal.valueOf(5))));
+    }
+
+    @Test
+    public void shouldCalculateLightingPerArea() {
+        when(roomService.calculateArea(any())).thenReturn(getArea());
+        when(roomService.calculateLighting(any())).thenReturn(getLighting());
+
+        Floor floor = getFloor();
+        BigDecimal lighting = floorService.calculateLighting(floor);
+        BigDecimal area = floorService.calculateArea(floor);
+        BigDecimal result = lighting.divide(area, MathContext.DECIMAL32);
+
+        assertThat("lightningPerArea", result, is(equalTo(BigDecimal.valueOf(1))));
+    }
+
+    @Test
+    public void shouldCalculateHeating() {
+        when(roomService.calculateHeating(any())).thenReturn(getHeating());
+
+        BigDecimal result = floorService.calculateHeating(getFloor());
+
+        assertThat("heating", result, is(equalTo(BigDecimal.valueOf(5))));
+    }
+
+    @Test
+    public void shouldCalculateHeatingPerVolume() {
+        when(roomService.calculateHeating(any())).thenReturn(getHeating());
+        when(roomService.calculateVolume(any())).thenReturn(getVolume());
+
+        Floor floor = getFloor();
+        BigDecimal heating = floorService.calculateHeating(floor);
+        BigDecimal volume = floorService.calculateVolume(floor);
+        BigDecimal result = heating.divide(volume, MathContext.DECIMAL32);
+
+        assertThat("heatingPerVolume", result, is(equalTo(BigDecimal.valueOf(1))));
     }
 
 }
