a8852c6f50b53b859da9a70c67ea5cbd6c0d7a67
jszymk <janusz.szymkowiak1@gmail.com>
2019-01-10 11:14:28 +0100

    test auth fix

--- src/test/java/integration/IntegrationTest.java
+++ src/test/java/integration/IntegrationTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,6 +20,7 @@ import javax.transaction.Transactional;
 @AutoConfigureMockMvc
 @TestPropertySource(
         locations = "classpath:application-test.properties")
+@WithMockUser(username = "admin", password = "admin", roles = {"USER", "ADMIN"})
 public abstract class IntegrationTest {
 
     @Autowired
